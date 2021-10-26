c1c330e149c984c5e3455f498a64b00369a2bbd4
asmi046 <asmi046@gmail.com>
2021-10-26 13:19:14 +0300

    Интерактивная заглушка

---
+++ index-2.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main class="page">
+
+	<section id="about-full" class="about-full">
+		<div class="container">
+			<div class="about-full__img">
+				<img src="<?php echo get_template_directory_uri();?>/img/logo.svg" alt="">
+				<h1>Сайт в разработке!</h1>
+			</div>
+		</div>
+	</section>  
+
+</main> 

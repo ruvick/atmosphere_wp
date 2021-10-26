796fee18f06985d8ef3dd09a3707eb53eff98b0d
Ruvick <rudikov.web@ya.ru>
2021-10-26 12:34:58 +0300

    Поправил заглушку

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 <main class="page">
 
-	<section id="about" class="about">
+	<section id="about-full" class="about-full">
 		<div class="container">
-			<div class="about__img">
+			<div class="about-full__img">
 				<img src="<?php echo get_template_directory_uri();?>/img/logo.svg" alt="">
-				<h1>Скоро открытие!</h1>
+				<h1>Сайт в разработке!</h1>
 			</div>
 		</div>
-	</section>
+	</section>  
 
-</main>
+</main> 

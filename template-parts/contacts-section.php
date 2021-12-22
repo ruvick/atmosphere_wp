9e6e8d68654dfdaf8de9fa13de9d1a90e9396ae3
Ruvick <rudikov.web@ya.ru>
2021-12-22 14:37:53 +0300

    Поправил контакты и соц сети

--- template-parts/contacts-section.php
+++ template-parts/contacts-section.php
@@ -106,10 +106,14 @@
 						if (!empty($mail)) { ?><li><a href="mailto:<? echo $mail; ?>" class="contacts-sec__email"><? echo $mail; ?></a></li><? } ?>
 					</ul>
 					<div class="contacts-sec__soc-block-icon soc-block-icon">
-						<a href="<?php echo carbon_get_theme_option('as_insta'); ?>" class="soc-block-icon-link soc-icon-1"></a>
-						<a href="<?php echo carbon_get_theme_option('as_vk'); ?>" class="soc-block-icon-link soc-icon-2"></a>
-						<a href="<?php echo carbon_get_theme_option('as_telegr'); ?>" class="soc-block-icon-link soc-icon-3"></a>
-						<a href="<?php echo carbon_get_theme_option('as_whatsapp'); ?>" class="soc-block-icon-link soc-icon-4"></a>
+					<? $insta = carbon_get_theme_option("as_insta"); if (!empty($insta)){?>
+						<a href="<? echo $insta; ?>" class="soc-block-icon-link soc-icon-1"></a><?}?>
+					<? $vk = carbon_get_theme_option("as_vk"); if (!empty($vk)){?>
+						<a href="<? echo $vk; ?>" class="soc-block-icon-link soc-icon-2"></a><?}?>
+					<? $telegr = carbon_get_theme_option("as_telegr"); if (!empty($telegr)){?>
+						<a href="<? echo $telegr; ?>" class="soc-block-icon-link soc-icon-3"></a><?}?>
+					<? $whatsapp = carbon_get_theme_option("as_whatsapp"); if (!empty($whatsapp)){?>
+						<a href="<? echo $whatsapp; ?>" class="soc-block-icon-link soc-icon-4"></a><?}?>
 					</div> 
 				</div>
 			</div>
